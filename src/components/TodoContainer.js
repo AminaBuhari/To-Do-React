5ece92a4e5a8bebd91c1a52b0d3f8735c9da58c2
buhariaminat <buhariaminat@yahoo.com>
2022-04-26 12:39:30 +0100

    Add components

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TodoContainer() {
+  return (
+    <>
+      <h1>Hello from Create React App</h1>
+      <p>I am in a React Component!</p>
+    </>
+  );
+}
+
+export default TodoContainer;
