cf31ab7b40763d829b00d68098ac9884256cfb94
buhariaminat <buhariaminat@yahoo.com>
2022-04-28 03:09:42 +0100

    Add hooks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import TodoContainer from './components/TodoContainer';
+import './index.scss';
+import TodoContainer from './classBased/components/TodoContainer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
